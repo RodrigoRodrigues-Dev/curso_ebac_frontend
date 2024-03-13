db2f309153a1d01a8278c321bdf7ac85e2054e2e
Rodrigo <rs9844660@gmail.com>
2024-03-13 11:01:36 -0300

    commit inicial

---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+const multiplicação = (n1: number, n2: number): number => n1 * n2;
+
+const saudacao = (nome: string): string => `Olá ${nome}`;
+
+console.log(multiplicação(15, 2));
+
+console.log(saudacao('Rodrigo'));
